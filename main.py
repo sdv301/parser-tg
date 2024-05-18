d65c4973e15ce0463695d338ded213ae395c594b
Your Name <you@example.com>
2024-05-18 14:25:30 +0700

    #несколько выходов для всех присутвующих ссылок

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import logging
 with open('web.txt', 'r', encoding='utf-8', errors='replace') as f:
     urls = [url.strip() for url in f.readlines()]
 
-for url in urls:
+for i, url in enumerate(urls):
     try:
         response = requests.get(url)
         response.raise_for_status() 
@@ -15,16 +15,13 @@ for url in urls:
         logging.error(f"Ошибка парсинга URL: {e}")
         continue
 
-html = response.text
-soup = LxmlSoup(html)  # создаём экземпляр класса LxmlSoup
+    html = response.text
+    soup = LxmlSoup(html)  # создаём экземпляр класса LxmlSoup
 
-links = soup.find_all('a') + soup.find_all('b') + soup.find_all('li') + soup.find_all('p') + soup.find_all('span') + soup.find_all('string')
+    links = soup.find_all('a') + soup.find_all('b') + soup.find_all('li') + soup.find_all('p') + soup.find_all('span') + soup.find_all('string') + soup.find_all("div", class_="products__item products__item_white")
 
-with open('output.txt', 'a', encoding='utf-8', errors='replace') as f:
-    for link in links:
-        name =link.text().strip()  
-        f.write(f"{name}\n")
-    print(f"Парсинг завершен {url}")
-       
-
-  
+    with open(f'output_{i+1}.txt', 'w', encoding='utf-8', errors='replace') as f:
+        for link in links:
+            name = link.text().strip()  
+            f.write(f"{name}\n")
+        print(f"Парсинг завершен {url}")
